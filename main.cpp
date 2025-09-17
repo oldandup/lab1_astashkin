84a4c133a3163bc2d753b44997d2c6bb0b01432d
unknown <denes2006@mail.ru>
2025-09-17 13:10:28 +0300

    ADD: pipe, cs, edit

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
+#include <limits>
 #include <string>
+#include <iomanip> // Для std::fixed и std::setprecision
 
 using namespace std;
 
 struct Pipe {
     string name;
-    float length;
-    int diametr;
+    double length;
+    double diametr;
     bool repair;
 };
 
@@ -18,123 +20,109 @@ struct Compress {
     bool working;
 };
 
-void Menu(Pipe t, Compress c) {
-    while (true)
-    {
-        cout << "\nChoose type:\n 1. Pipe \n 2. Compress\n 3. Show All\n";
-        int type;
-        cin >> type;
-        if (cin.fail()) {
-            cout << "Error!" << endl;
-            cout << "Write correct number (1-3)" << endl;
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+void Menu(Pipe& t, Compress& c) { // Передача по ссылке
+    int choice;
+    while (true) {
+        cout << "\nMenu:\n";
+        cout << "1. Add pipe\n";
+        cout << "2. Add CS\n";
+        cout << "3. View objects\n";
+        cout << "4. Exit\n";
+        cout << "Choose an option: ";
+        cin >> choice;
+
+        switch (choice) {
+        case 1: {
+            cout << "Pipe parameters:\n";
+            cout << "Enter name: ";
+            cin.ignore(); // Очищаем буфер перед getline
+            getline(cin, t.name);
+
+
+            cout << "Enter length: ";
+            cin >> t.length;
+            if (cin.fail() || t.length <= 0) { // Проверка на ошибку и валидация
+                cout << "Error: Invalid length. Please enter a positive number.\n";
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                break;
+            }
+
+            cout << "Enter diameter: "; // Исправлено "diametr" на "diameter"
+            cin >> t.diametr;
+            if (cin.fail() || t.diametr <= 0) { // Проверка на ошибку и валидация
+                cout << "Error: Invalid diameter. Please enter a positive number.\n";
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                break;
+            }
+
+            cout << "On repair? (0 - no, 1 - yes) ";
+            cin >> t.repair;
+            if (cin.fail()) {
+                cout << "Error: Invalid input. Please enter 0 or 1.\n";
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                break;
+            }
+            break;
         }
-        switch (type)
-        {
-        case 1:
-            cout << "Choose option:\n 1. Add pipe \n 2. Pipe lenght\n 3. Pipe diametr\n 4. Repairing\n";
-            int option;
-            cin >> option;
-            if (cin.fail()) {
-                cout << "Error!" << endl;
-                cout << "Write correct number (1-4)" << endl;
+        case 2: {
+            cout << "CS parameters:\n";
+            cout << "Enter name: ";
+            cin.ignore(); // Очищаем буфер перед getline
+            getline(cin, c.name);
+
+            cout << "Enter quantity: ";
+            cin >> c.count;
+            if (cin.fail() || c.count < 0) {
+                cout << "Error: Invalid input. Please enter a non-negative number.\n";
                 cin.clear();
                 cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                break;
             }
-            switch (option)
-            {
-            case 1:
-                cout << "Enter pipe name:";
-                cin >> t.name;
+
+            cout << "Enter quantity on working: ";
+            cin >> c.count_working;
+            if (cin.fail() || c.count_working < 0 || c.count_working > c.count) {
+                cout << "Error: Invalid input. Please enter a non-negative number less than or equal to quantity.\n";
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
                 break;
-            case 2:
-                cout << "Enter pipe lenght (km): ";
-                cin >> t.length;
-                if (cin.fail()) {
-                    cout << "Error!" << endl;
-                    cout << "Write correct number" << endl;
-                    cin.clear();
-                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-                }
-                break;
-            case 3:
-                cout << "Enter pipe diametr (mm): ";
-                cin >> t.diametr;
-                if (cin.fail()) {
-                    cout << "Error!" << endl;
-                    cout << "Write correct number" << endl;
-                    cin.clear();
-                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-                }
-                break;
-            case 4:
-                cout << "Is pipe on repair? (0 - no, 1 - yes)";
-                cin >> t.repair;
-                if (cin.fail()) {
-                    cout << "Error!" << endl;
-                    cout << "Write 0 or 1" << endl;
-                    cin.clear();
-                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-                }
+            }
+
+            cout << "Enter classification: ";
+            cin.ignore(); // Очищаем буфер перед getline
+            getline(cin, c.classification);
+
+            cout << "Is CS working? (0 - no, 1 - yes) ";
+            cin >> c.working;
+            if (cin.fail()) {
+                cout << "Error: Invalid input. Please enter 0 or 1.\n";
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
                 break;
             }
             break;
-        case 2:
-            cout << "Choose option:\n 1. Add CS \n 2. Quantity of CS \n 3. Quantity working\n 4. Classification\n 5. Working\n";
-            int option2;
-            cin >> option2;
-            if (cin.fail()) {
-                cout << "Error!" << endl;
-                cout << "Write correct number (1-5)" << endl;
-                cin.clear();
-                cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            }
-            switch (option2)
-            {
-            case 1:
-                cout << "Enter CS name: ";
-                cin >> c.name;
-                break;
-            case 2:
-                cout << "Enter quantity of enterprises: ";
-                cin >> c.count;
-                if (cin.fail()) {
-                    cout << "Error!" << endl;
-                    cout << "Write correct number" << endl;
-                    cin.clear();
-                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-                }
-                break;
-            case 3:
-                cout << "Enter quantity of working enterprises: ";
-                cin >> c.count_working;
-                if (cin.fail()) {
-                    cout << "Error!" << endl;
-                    cout << "Write correct number" << endl;
-                    cin.clear();
-                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-                }
-                break;
-            case 4:
-                cout << "Enter classification: ";
-                cin >> c.classification;
-                break;
-            case 5:
-                cout << "Is CS working? (0 - no, 1 - yes) ";
-                cin >> c.working;
-                if (cin.fail()) {
-                    cout << "Error!" << endl;
-                    cout << "Write 0 or 1" << endl;
-                    cin.clear();
-                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-                }
-                break;
-            }
-            break;
+        }
         case 3:
-            cout << "\nPipe parameters: \n" << "Name: " << t.name << "\nLength: " << t.length << "\nDiametr: " << t.diametr << "\nOn repair: " << t.repair;
-            cout << "\n\nCS parameters: \n" << "Name: " << c.name << "\nQuantity: " << c.count << "\nOn working: " << c.count_working << "\nClassification: " << c.classification << "\nWorking : " << c.working;
+             cout << "\nPipe parameters:\n"
+                  << "Name: " << t.name << "\n"
+                  << "Length: " << fixed << setprecision(2) << t.length << "\n" // Форматированный вывод с двумя знаками после запятой
+                  << "Diameter: " << t.diametr << "\n" // Исправлено "Diametr" на "Diameter"
+                  << "On repair: " << (t.repair ? "Yes" : "No") << "\n";
+
+             cout << "\nCS parameters:\n"
+                  << "Name: " << c.name << "\n"
+                  << "Quantity: " << c.count << "\n"
+                  << "On working: " << c.count_working << "\n"
+                  << "Classification: " << c.classification << "\n"
+                  << "Working: " << (c.working ? "Yes" : "No") << "\n";
+            break;
+        case 4:
+            return;
+        default:
+            cout << "Invalid option. Please try again.\n";
             break;
         }
     };
@@ -144,5 +132,19 @@ int main()
 {
     Pipe truba;
     Compress ceh;
+
+    // Инициализация значений по умолчанию
+    truba.name = "Unknown";
+    truba.length = 0.0;
+    truba.diametr = 0.0;
+    truba.repair = false;
+
+    ceh.name = "Unknown";
+    ceh.count = 0;
+    ceh.count_working = 0;
+    ceh.classification = "Unknown";
+    ceh.working = false;
+
+
     Menu(truba, ceh);
 }
